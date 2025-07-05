c2bfe1ef1dd0d7b1387ec5b5a90a72a4bbcbf557
jhulyanne <jhulyannedeoliveira@gmail.com>
2025-07-05 09:12:32 -0300

    :sparkles: feat: add  Book, Loan and User interfaces to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,4 +5,20 @@ export interface Book {
   publisher: string;
   genre: string;
   available: boolean;
-}
+};
+
+export interface Loan {
+  id: number;
+  user_cpf: string;
+  book_id: number;
+  loan_date: string;
+  return_date?: string;
+};
+
+export interface User {
+  id: number,
+  cpf: string,
+  full_name: string,
+  phone_number: string,
+  address: string
+};
